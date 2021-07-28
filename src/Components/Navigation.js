ac55e06ff6a36aae0c9392793cbdc90c33e23bed
AmanChaudhary1998 <chaudharyaman50@yahoo.com>
2021-07-28 09:39:57 +0530

    navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,36 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = ()=>{
     return(
         <>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Render.io</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="#">Home</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="#">About</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="#">Team</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="#">Technology</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="#">Contact</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="#">Render.io</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="#">Home</Link>
+              </li>
+              <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="#">About</Link>
+              </li>
+              <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="#">Team</Link>
+              </li>
+              <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="#">Technology</Link>
+              </li>
+              <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="#">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </nav>
         </>
     )
 }
